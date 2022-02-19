e85356c6526248626addc93334b80128bee9f213
EimantasBlazevicius <eima.blaz@gmail.com>
2022-02-19 10:11:06 +0200

    base html added

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 
 @app.route("/sum/<num1>/<num2>")
-def another(num1, num2):
-    sum = int(num1) - int(num2)
-    return f"<h2>Hello, World from another page! {sum}</h2>"
+def addition(num1, num2):
+    sum = int(num1) + int(num2)
+    return render_template("calculator.html", sum=sum)
 
 
 if __name__ == "__main__":
