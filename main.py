cc37b60949e26db122f75e7703a1d1b8b26b1b4a
EimantasBlazevicius <eima.blaz@gmail.com>
2022-02-19 11:24:19 +0200

    a few more things and pip freeze

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from requests import get
 
 app = Flask(__name__)
 
@@ -12,6 +13,27 @@ def home():
 def addition(num1, num2):
     sum = int(num1) + int(num2)
     return render_template("calculator.html", sum=sum)
+
+
+@app.route("/users")
+def users():
+    people = [{"id": 1, "name": "Mark", "moto": "Mark my words"},
+              {"id": 2, "name": "Jonas", "moto": "Jonines yra gerai"},
+              {"id": 3, "name": "Ugnius", "moto": "Viskas vistiek sudegs"}
+              ]
+    type = ""
+    return render_template("users.html", people=people, type=type)
+
+
+@app.route("/calculator", methods=["GET", "POST"])
+def normal_addition():
+    the_sum = 0
+    if request.method == "POST":
+        num1 = int(request.form['number1'])
+        num2 = int(request.form['number2'])
+        the_sum = num1 + num2
+
+    return render_template("normal_calc.html", sum=the_sum)
 
 
 if __name__ == "__main__":
